133b7d5cba2ebd2419e6eee8839e5cfcffec7e7d
Sebastian Erdweg <seba@informatik.uni-marburg.de>
2012-12-18 11:12:53 +0100

    no need to sync private non-static fields

--- jsglr-layout/src/org/spoofax/jsglr_layout/client/PathListPool.java
+++ jsglr-layout/src/org/spoofax/jsglr_layout/client/PathListPool.java
@@ -21,51 +21,41 @@ public class PathListPool {
 		// singleton
 	}
 	
-	private static Object getSyncRoot() {
-		return PathListPool.class;
-	}
-	
 	public static PathListPool getInstance() {
 		return new PathListPool();
 	}
 	
 	public PooledPathList create() {
-		synchronized (getSyncRoot()) {
-			if(asyncP0.usage == 0)
-				return asyncP0.start();
-			if(asyncP1.usage == 0)
-				return asyncP1.start();
-			if(asyncP2.usage == 0)
-				return asyncP2.start();
-			if(asyncP3.usage == 0)
-				return asyncP3.start();
-			if(asyncP4.usage == 0)
-				return asyncP4.start();
-		
-			asyncCacheMisses++;
-			PooledPathList paths = new PooledPathList(512, false);
-			endCreate(paths);
+		if(asyncP0.usage == 0)
 			return asyncP0.start();
-		}
+		if(asyncP1.usage == 0)
+			return asyncP1.start();
+		if(asyncP2.usage == 0)
+			return asyncP2.start();
+		if(asyncP3.usage == 0)
+			return asyncP3.start();
+		if(asyncP4.usage == 0)
+			return asyncP4.start();
+	
+		asyncCacheMisses++;
+		PooledPathList paths = new PooledPathList(512, false);
+		endCreate(paths);
+		return asyncP0.start();
 	}
 	
 	public void resetPerformanceCounters() {
-		synchronized (getSyncRoot()) {
-			asyncCacheMisses = 0;
-		}
+		asyncCacheMisses = 0;
 	}
 
 	public void endCreate(PooledPathList paths) {
-		synchronized (getSyncRoot()) {
-			paths.reset();
-			if (asyncP0 == paths || asyncP1 == paths || asyncP2 == paths
-					|| asyncP3 == paths || asyncP4 == paths)
-				return;
-			asyncP4 = asyncP3;
-			asyncP3 = asyncP2;
-			asyncP2 = asyncP1;
-			asyncP1 = asyncP0;
-			asyncP0 = paths;
-		}
+		paths.reset();
+		if (asyncP0 == paths || asyncP1 == paths || asyncP2 == paths
+				|| asyncP3 == paths || asyncP4 == paths)
+			return;
+		asyncP4 = asyncP3;
+		asyncP3 = asyncP2;
+		asyncP2 = asyncP1;
+		asyncP1 = asyncP0;
+		asyncP0 = paths;
 	}
 }
