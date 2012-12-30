8fb75ee08628370dcbe22f96c162e8a30e526066
Sebastian Erdweg <seba.github@gmail.com>
2012-12-30 23:45:05 +0100

    rm jsglr-orig from plugin branch

--- jsglr-orig/scripts/bundle.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash -x
-
-mkdir -p staging/
-rsync -r ../src/ staging/
-rsync -r ../war/WEB-INF/classes/ staging/
-jar cvf jsglr-gwt-$(date +"%Y-%m-%d").jar -C staging/ .
-  
--- jsglr-orig/src/org/spoofax/jsglr_orig/client/IRecoveryParser.java
+++
@@ -1,6 +0,0 @@
-package org.spoofax.jsglr_orig.client;
-
-public interface IRecoveryParser {
-    IRecoveryResult recover(String text) throws Exception;
-    IRecoveryResult recover(String text, String startSymbol) throws Exception;
-}
