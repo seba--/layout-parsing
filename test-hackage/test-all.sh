fb3b1164434fee082d7456510457171b2c3e0daf
Sebastian Erdweg <seba.github@gmail.com>
2013-04-29 18:24:50 +0200

    add javassist to class path

--- test-hackage/test-all.sh
+++ test-hackage/test-all.sh
@@ -3,6 +3,6 @@
 JSGLR_ORIG=../jsglr-orig/bin
 JSGLR_LAYOUT=../jsglr-layout/bin
 NORMALIZE=../haskell-parsetree-normalize/bin
-PLUGINS=junit.jar:../org.spoofax.terms_1.0.0.201212201448.jar:strategoxt.jar
+PLUGINS=junit.jar:../org.spoofax.terms_1.0.0.201212201448.jar:strategoxt.jar:../jsglr-layout/javassist.jar
 
 java -Xss16m -Xmx512m -cp bin:$JSGLR_ORIG:$JSGLR_LAYOUT:$NORMALIZE:$PLUGINS org.spoofax.jsglr.tests.haskell.TestAllPackages
