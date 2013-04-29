883b7002a4b23871692a805022838ec16b7eb3f4
Sebastian Erdweg <seba.github@gmail.com>
2013-04-29 08:26:28 +0200

    memory flags

--- test-hackage/test-all.sh
+++ test-hackage/test-all.sh
@@ -5,4 +5,4 @@ JSGLR_LAYOUT=../jsglr-layout/bin
 NORMALIZE=../haskell-parsetree-normalize/bin
 PLUGINS=junit.jar:../org.spoofax.terms_1.0.0.201212201448.jar:strategoxt.jar
 
-java -cp bin:$JSGLR_ORIG:$JSGLR_LAYOUT:$NORMALIZE:$PLUGINS org.spoofax.jsglr.tests.haskell.TestAllPackages
+java -Xss16m -Xmx512m -cp bin:$JSGLR_ORIG:$JSGLR_LAYOUT:$NORMALIZE:$PLUGINS org.spoofax.jsglr.tests.haskell.TestAllPackages
